5000f4b0b2f2417ce18edc1b9f97326258013d86
GrigoriyBabikhin <Grigoriy.Babikhin@gmail.com>
2023-06-20 23:23:49 +0500

    Codewars

--- JavaScript/Fundamentals Return.js
+++ JavaScript/Fundamentals Return.js
@@ -1,26 +1,27 @@
 function add(a,b){
-    return
+    return a + b;
 }
 
 function divide(a,b){
-    return
+    return a / b;
 }
 
 function multiply(a,b){
-    return
+    return a * b;
 }
 
 function mod(a,b){
-    return
+    return a % b
 }
 
 function exponent(a,b){
-    return
+    return a ** b
 }
 
 function subt(a,b){
-    return
+    return a - b
 }
+
 /*
 Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
 
