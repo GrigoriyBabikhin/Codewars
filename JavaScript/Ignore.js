87feca6c65d38ebc1e6562436830c4f4938e0c63
GrigoriyBabikhin <Grigoriy.Babikhin@gmail.com>
2023-07-20 01:12:06 +0500

    js

--- JavaScript/Ignore.js
+++ JavaScript/Ignore.js
@@ -1,14 +1,7 @@
 function solution(a, b) {
-    let s = '', l = '';//создали переменную.
 
-    if (a.length > b.length) {//то b+a+b.
-        l = a;
-        s = b;
-    } else {// в остальных случаях a+b+a.
-        s = a;
-        l = b;
-    }
-    return `${s}${l}${s}`//выводим строку.
+    if (a.length > b.length) return `${b}${a}${b}`
+    else return `${a}${b}${a}`
 }
 
-console.log(solution('45','1'))
+console.log(solution('44','1'))
