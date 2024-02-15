dda247ae6f8f1fa86d7f922972f795037663c9e8
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-02-16 00:00:43 +0500

    Array

--- JavaScript/Arrays/Find Multiples of a Number.js
+++ JavaScript/Arrays/Find Multiples of a Number.js
@@ -2,18 +2,18 @@
 // поскольку 2, 4 и 6 кратны числам от 2 до 6.
 
 //Первое решение
-// function findMultiples(integer, limit) {
-//   const arr = [];
-//     for (let i = integer; i <= limit; i++){
-//       if(i % integer === 0) {
-//         arr.push(i)}
-//     }
-//     return arr
-// }
-//
-// console.log(findMultiples(4, 27))//[ 4, 8, 12, 16, 20, 24 ]
+function findMultiples(integer, limit) {
+  const arr = [];
+    for (let i = integer; i <= limit; i++){
+      if(i % integer === 0) {
+        arr.push(i)}
+    }
+    return arr
+}
 
-//лучшее решение
+console.log(findMultiples(4, 27))//[ 4, 8, 12, 16, 20, 24 ]
+
+// лучшее решение
 // function findMultiples(int,limit){
 //     let result = []
 //
