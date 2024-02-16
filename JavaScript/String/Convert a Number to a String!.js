59e262b9c1da9560a990d9904e9d299d88b4d7d0
Grigoriy Babikhin <108750257+GrigoriyBabikhin@users.noreply.github.com>
2024-02-16 22:50:45 +0500

    String

---
+++ JavaScript/String/Convert a Number to a String!.js
@@ -0,0 +1,14 @@
+//Вернуть из числа строку.
+
+
+//Мое решение
+// function numberToString(num) {
+//     return String(num)
+// }
+
+//Лучшее решение
+function numberToString(num) {
+    return num.toString();
+}
+const num = 666;
+console.log(numberToString(num))
